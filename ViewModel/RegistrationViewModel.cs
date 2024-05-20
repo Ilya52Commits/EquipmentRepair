115850121c8398606d7fb150afdb7b257ae66b02
Ilya <krasnenkov.ilya@inbox.ru>
2024-05-20 23:49:31 +0400

    Прописал логику для RegistrationViewModel

--- ViewModel/RegistrationViewModel.cs
+++ ViewModel/RegistrationViewModel.cs
@@ -1,8 +1,58 @@
 ﻿namespace EquipmentRepair.ViewModel;
 internal sealed partial class RegistrationViewModel : BaseViewModel
 {
-  private DbContext _dbContext;
+  private readonly DbContext _dbContext;
 
+  private string _passwordContent;
+  private string _confPasswordContent; 
+  
+  private string _login;
+  public string Login
+  {
+    get => _login;
+    set
+    {
+      _login = value; 
+      OnPropertyChanged();
+    }
+  }
 
+  private string _email;
+  public string Email
+  {
+    get => _email;
+    set
+    {
+      _email = value; 
+      OnPropertyChanged();
+    }
+  }
+  
+  private string _password;
+  public string Password
+  {
+    get => _password;
+    set
+    {
+      _password = _passwordContent;
+      OnPropertyChanged();
+    }
+  }
+
+  private string _confPassword;
+  public string ConfPassword
+  {
+    get => _confPassword;
+    set
+    {
+      _confPassword = _confPasswordContent;
+      OnPropertyChanged();
+    }
+  }
+  
+  public RegistrationViewModel(string password, string confPassword)
+  {
+    
+  }
 }
 
--- Viws/RegistrationView.xaml.cs
+++ Viws/RegistrationView.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Net;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using EquipmentRepair.ViewModel;
 
 namespace EquipmentRepair.Viws
 {
@@ -23,6 +12,11 @@ namespace EquipmentRepair.Viws
     public RegistrationView()
     {
       InitializeComponent();
+
+      var password = new NetworkCredential(string.Empty, PasswordBoxName.SecurePassword).Password;
+      var confPassword = new NetworkCredential(string.Empty, ConfPasswordBoxName.SecurePassword).Password;
+      
+      DataContext = new RegistrationViewModel(password, confPassword);
     }
   }
 }
